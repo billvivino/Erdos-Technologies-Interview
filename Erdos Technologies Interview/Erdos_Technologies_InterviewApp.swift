9be171e3c70ffa6e85a1faccb1c17ec4e83467bb
billvivino <billvivino@gmail.com>
2023-02-15 17:47:10 -0500

    Initial Commit

---
+++ Erdos Technologies Interview/Erdos_Technologies_InterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Erdos_Technologies_InterviewApp.swift
+//  Erdos Technologies Interview
+//
+//  Created by Bill Vivino on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Erdos_Technologies_InterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
